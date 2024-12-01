884ab3f981a14a8f1434a7a8650e51e6968a989a
AtharPnh <ath.pnh@gmail.com>
2024-12-01 19:01:46 +0800

    initial project

---
+++ src/main/java/com/athar/food_reservation/FoodReservationApplication.java
@@ -0,0 +1,13 @@
+package com.athar.food_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodReservationApplication.class, args);
+	}
+
+}
